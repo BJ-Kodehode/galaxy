eeaa8865aeb8f121766af2e651ace95fccbb451e
BJ-Kodehode <bernt25sfj@gmail.com>
2025-07-03 10:48:27 +0200

    Fjernrnet en overskrift

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -4,7 +4,6 @@ import GalleryContent from './GalleryContent';
 export default function GalleryPage() {
   return (
     <main className="container mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold mb-6">Galleri: Melkeveien & Planeter</h1>
       <Suspense fallback={<div>Laster galleri...</div>}>
         <GalleryContent />
       </Suspense>
